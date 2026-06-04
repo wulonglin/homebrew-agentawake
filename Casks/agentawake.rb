bb0bb0d74890feccf832c40d6e58733202efe65f
wulonglin <wulonglin@live.com>
2026-06-04 12:34:10 +0800

    Update to v1.1.0

--- Casks/agentawake.rb
+++ Casks/agentawake.rb
@@ -1,5 +1,5 @@
 cask "agentawake" do
-  version "1.0.0"
+  version "1.1.0"
   sha256 "73e24d7f9a891acb3c97ed9a4dbd46b71ce33c186a9ba478506a12a2f5ebe7f2"
 
   url "https://github.com/wulonglin/AgentAwake/releases/download/v#{version}/AgentAwake-#{version}.dmg"
